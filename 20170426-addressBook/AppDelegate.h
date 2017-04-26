3222ffb7d420ece9046f7cdb00d361c357ed1178
Dmitry A. Zvorykin <dmitry.zvorikin@gmail.com>
2017-04-26 21:57:48 +0300

    Initial Commit

---
+++ 20170426-addressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20170426-addressBook
+//
+//  Created by Admin on 26/04/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20170426-addressBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20170426-addressBook
+//
+//  Created by Admin on 26/04/2017.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
